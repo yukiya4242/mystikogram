9017468f79365b65f1bc40df61c47918a247ba55
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-04-04 05:19:09 +0000

    add_job_MessageBroadcast

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,29 +1,22 @@
 import consumer from "./consumer"
 
-const appRoom = consumer.subscriptions.create("RoomChannel",{
-
 consumer.subscriptions.create("RoomChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
   },
-
-
-
-
 
   disconnected() {
     // Called when the subscription has been terminated by the server
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    return alert(data['message']);
   },
 
-  speak: function() {
-    return this.perform('speak');
+   speak: function(message) {
+    return this.perform('speak', {message: message});
   }
 });
-
 
 window.addEventListener("keypress", function(e) {
   if (e.keyCode === 13) {
@@ -32,3 +25,7 @@ window.addEventListener("keypress", function(e) {
     e.preventDefault();
   }
 })
+
+
+
+
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,5 @@
 class Message < ApplicationRecord
+after_create_commit { MessageBroadcastJob.perform_later self }
+
+
 end
